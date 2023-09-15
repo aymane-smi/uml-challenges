813d2bec64bfdf4572c3bae88a214a175dc6ad3e
aymane-smi <aymanebel2@outlook.fr>
2023-09-15 15:15:55 +0100

    update

--- challenge2/src/main/java/com/example/Category.java
+++ challenge2/src/main/java/com/example/Category.java
@@ -1,7 +1,10 @@
 package com.example;
 
+import java.util.List;
+
 public class Category {
     private int idCategory;
     private int nbrBeds = 0;
     private float price = 0;
+    private List<Booking> bookings = null;
 }
